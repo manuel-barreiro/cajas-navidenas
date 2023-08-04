d2663aebc70fe1c9219849e39419b7391131fac1
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-08-04 09:56:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,205 @@
+
+// const par = document.querySelector(".glide__slide");
+// const boton5 = document.querySelector('#boton2')
+
+// par.addEventListener("resize", resizeListener );
+
+// function resizeListener() {
+//   let  width = par.offsetWidth;
+//   console.log(width)
+//   if( window.matchMedia("(max-width: 760px)")) {
+//     boton5.style.marginLeft = `${width /2  - 125}px`;
+//   } else {
+   
+//   boton5.style.marginLeft = `${width /2  - 150}px`;
+//   }
+// }
+
+
+// const par = document.querySelector(".glide__slide");
+// const boton5 = document.querySelector('#boton2')
+
+
+
+// var inverval_timer;
+
+  
+// inverval_timer = setInterval(function() { let  width = par.offsetWidth;
+//       console.log(width)
+//       if( window.innerWidth < 760) {
+//         boton5.style.marginLeft = `${width /2  - 125}px`;
+//        } else {
+       
+//       boton5.style.marginLeft = `${width /2  - 150}px`;
+//        }
+    
+// }, 2000);
+
+
+
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".links");
+  const navLinks = document.querySelectorAll(".links li");
+  console.log(navLinks);
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+        console.log(link.style.animation);
+      } else {
+        link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7}s`;
+      }
+      console.log(index / 7 + 1);
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
+
+document.getElementById("arrow").addEventListener("click", () => {
+  document
+    .querySelector(".cajaroja")
+    .scrollIntoView({ behavior: "smooth", block: "end" });
+});
+document.getElementById("somos").addEventListener("click", () => {
+  document
+    .querySelector(".ofrecemos")
+    .scrollIntoView({ behavior: "smooth", block: "end" });
+});
+
+document.getElementById("ofrecemos").addEventListener("click", () => {
+  document
+    .querySelector(".llegamos")
+    .scrollIntoView({ behavior: "smooth", block: "end" });
+});
+
+document.getElementById("llegamos").addEventListener("click", () => {
+  document
+    .querySelector(".empresas")
+    .scrollIntoView({ behavior: "smooth", block: "end" });
+});
+
+// const img = document.getElementById('imgprin')
+// const boton = document.getElementById('boton2')
+// let width = img.offsetWidth;
+// console.log(width)
+// boton.style.marginLeft = width/2 + "px"
+
+// navNosotros.forEach((item) => item.addEventListener('click',() =>{
+//   let el = document.querySelector('.nosotros')
+//    el.scrollIntoView({behavior:"smooth", block: "end"})
+
+// }))
+// window.onload = function () {
+//   var carouselSlide = document.querySelector(".carousel-slide");
+//   var carouselImg = document.querySelectorAll(".carousel-slide img");
+
+//   let count = 1;
+
+//   var size = window.innerWidth;
+
+  //  carouselSlide.style.transform = "translateX(" + -size * count + "px)";
+
+//   console.log(size);
+
+//   nextBtn.addEventListener("click", () => {
+//     if(count >= carouselImg.length-1) return;
+//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//     count++;
+//     carouselSlide.style.transform = "translateX(" + -size * count + "px)";
+//   });
+
+//   prevBtn.addEventListener("click", () => {
+//     if(count <= 0) return;
+//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//     count--;
+//     carouselSlide.style.transform = "translateX(" + -size * count + "px)";
+//   });
+
+//   carouselSlide.addEventListener("transitionend", () => {
+//     console.log(carouselImg[count].id);
+//     if (carouselImg[count].id == "lastClone") {
+//       carouselSlide.style.transition = "none";
+//       count = carouselImg.length - 2;
+//       carouselSlide.style.transform = "translatex(" + -size * count + "px)";
+//     }
+//   });
+
+//   carouselSlide.addEventListener("transitionend", () => {
+//     console.log(carouselImg[count].id);
+//     if (carouselImg[count].id == "firstClone") {
+//       carouselSlide.style.transition = "none";
+//       count = carouselImg.length - count;
+//       carouselSlide.style.transform = "translatex(" + -size * count + "px)";
+//     }
+//   });
+
+//   function yourFunction(){
+//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+//     count++;
+//     carouselSlide.style.transform = "translateX(" + -size * count + "px)";
+
+// yourFunction();
+
+
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.getElementById("arrow").addEventListener("click", () => {
+      document
+        .querySelector(".cajaroja")
+        .scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+    document.getElementById("somos").addEventListener("click", () => {
+      document
+        .querySelector(".ofrecemos")
+        .scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+    
+    document.getElementById("ofrecemos").addEventListener("click", () => {
+      document
+        .querySelector(".llegamos")
+        .scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+    
+    document.getElementById("llegamos").addEventListener("click", () => {
+      document
+        .querySelector(".empresas")
+        .scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+  } else {
+    document.getElementById("arrow").addEventListener("click", () => {
+      document
+        .querySelector(".cajaroja")
+        .scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+    document.getElementById("somos").addEventListener("click", () => {
+      document
+        .querySelector(".ofrecemos")
+        .scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+    
+    document.getElementById("ofrecemos").addEventListener("click", () => {
+      document
+        .querySelector(".ofrecemos")
+        .scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+    
+    document.getElementById("llegamos").addEventListener("click", () => {
+      document
+        .querySelector(".footer")
+        .scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+  }
+}
+
+var x = window.matchMedia("(max-width: 760px)")
+myFunction(x) // Call listener function at run time
+
---
+++ contact-form-17/php/email2.php
@@ -0,0 +1,102 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+
+
+
+
+//Import PHPMailer class into the global namespace
+
+
+$msg = '';
+//Don't run this unless we're handling a form submission
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
+
+   
+
+    //Create a new PHPMailer instance
+    $phpmailer = new PHPMailer();
+    
+    $phpmailer->isSMTP();
+    $phpmailer->Host = 'smtp.office365.com';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->AuthType = 'LOGIN';
+    $phpmailer->Port = 587;
+    $phpmailer->Username = 'phpsecondary@outlook.com';
+    $phpmailer->Password = 'Boca159951!';
+    $phpmailer->SMTPSecure = 'tls';
+
+    
+    $phpmailer->setFrom('phpsecondary@outlook.com', 'Presupuesto nuevo por pagina web');
+   
+    // contacto@TusCajasNavidenas.com.ar
+   
+        $phpmailer->addAddress('contacto@TusCajasNavidenas.com.ar');
+    
+    
+    if ($phpmailer->addReplyTo($_POST['email'], $_POST['name'])) {
+        $phpmailer->Subject = 'PHPMailer contact form';
+        //Keep it simple - don't use HTML
+        $phpmailer->isHTML(false);
+        //simple message body
+        $phpmailer->Body = <<<EOT
+Email: {$_POST['email']}
+Nombre: {$_POST['name']}
+Telefono: {$_POST['phone']}
+Empresa: {$_POST['company']}
+Message: {$_POST['message']}
+EOT;
+        //Send the message, check for errors
+        if (!$phpmailer->send()) {
+            
+            echo 'Mailer error: ' . $mail->ErrorInfo;
+            $msg = 'Hubo un error, contactese a traves de Whatsapp o por mail.';
+        } else {
+            $msg = 'Mensaje enviado!.';
+        }
+    } else {
+        $msg = 'Email invalido, chequee el formato de su mail.';
+    }
+}
+?>
+
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700' rel='stylesheet' type='text/css'>
+	<style>
+		@import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+		@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+	</style>
+	<link rel="stylesheet" href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css">
+    
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+</head>
+<body>
+	<header class="site-header" id="header">
+		<h1 class="site-header__title" data-lead-id="site-header-title">Gracias!</h1>
+	</header>
+
+	<div class="main-content">
+		<i class="fa fa-check main-content__checkmark" id="checkmark"></i>
+        <?php if (!empty($msg)) {
+    echo "<h2>$msg</h2>";
+} ?>
+		<p class="main-content__body" data-lead-id="main-content-body">En la brevedad nos estaremos contactando con usted!</p>
+	</div>
+
+	<footer class="site-footer" id="footer">
+		<p class="site-footer__fineprint" id="fineprint">© 2022 Tus cajas navideñas</p>
+	</footer>
+</body>
+</html>
